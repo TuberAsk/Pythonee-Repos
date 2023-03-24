3be6882370a0b38ccd2f7b6f3be67c4985bc1185
Pythoneer <125912730+TuberAsk@users.noreply.github.com>
2023-03-24 23:10:05 +0100

    Update starter.py

--- starter.py
+++ starter.py
@@ -22,3 +22,14 @@ result = subNumbers(a, b)
 print("subtraction of ", a, " and ", b, " is = ", result)
 
 #That code is not mine but it is a example of a def function!
+#To start of with a discord bot you will need Visual Studio Code and discord.py 2.2.2 or any other versions!
+import discord #Imports the discord module.
+from discord.ext import commands #Imports discord extensions.
+
+#The below code verifies the "client".
+intents = discord.Intents().all()
+client = commands.Bot(command_prefix='?', intents=intents)
+client.run("YOUR BOT TOKEN!")
+
+#Just a simple starter script for you bot to make it online!
+#Check out Pythoneer on YouTube!
